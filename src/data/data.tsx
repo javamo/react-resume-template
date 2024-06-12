78e7087600b96e221e8b84491a3be1b0b7c04138
Jairo Vasquez <jairo.vasquez@gmail.com>
2024-06-12 18:01:00 -0400

    personal configs

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -11,7 +11,6 @@ import {
 import GithubIcon from '../components/Icon/GithubIcon';
 import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
-import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
@@ -44,8 +43,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Javamo Tech',
+  description: "Jairo Vásquez developer website",
 };
 
 /**
@@ -69,18 +68,17 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Jairo Vásquez`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a Medellín based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
+        at <strong className="text-stone-100">Atlas Health</strong> helping hospital patients get financial aid through Philantropic sources.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        In my free time time, you can catch me training in <strong className="text-stone-100">Triathlon</strong>,
+        playing my <strong className="text-stone-100">Piano</strong>, or more importantly{' '}
+        <strong className="text-stone-100">playing with my daughter</strong>.
       </p>
     </>
   ),
@@ -104,16 +102,15 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `I love computers and the impact they have in our current world. I believe technology
+    can transform societies and more importantly, minds and lives.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'Medellín, Colombia', Icon: MapIcon},
+    {label: 'Age', text: '37', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Colombian', Icon: FlagIcon},
+    {label: 'Interests', text: 'Triathlon, Piano, Parenting', Icon: SparklesIcon},
+    {label: 'Study', text: 'EAFIT University', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'Atlas Health', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -125,16 +122,16 @@ export const skills: SkillGroup[] = [
     name: 'Spoken languages',
     skills: [
       {
-        name: 'English',
+        name: 'Spanish',
         level: 10,
       },
       {
-        name: 'French',
-        level: 4,
+        name: 'English',
+        level: 8,
       },
       {
-        name: 'Spanish',
-        level: 3,
+        name: 'Portuguese',
+        level: 6,
       },
     ],
   },
@@ -159,11 +156,11 @@ export const skills: SkillGroup[] = [
     name: 'Backend development',
     skills: [
       {
-        name: 'Node.js',
+        name: 'Python',
         level: 8,
       },
       {
-        name: 'Rust',
+        name: 'Javascript',
         level: 5,
       },
       {
@@ -173,19 +170,19 @@ export const skills: SkillGroup[] = [
     ],
   },
   {
-    name: 'Mobile development',
+    name: 'Databases',
     skills: [
       {
-        name: 'React Native',
+        name: 'MySQL',
         level: 9,
       },
       {
-        name: 'Flutter',
-        level: 4,
+        name: 'Google Datastore',
+        level: 6,
       },
       {
-        name: 'Swift',
-        level: 3,
+        name: 'MongoDB',
+        level: 4,
       },
     ],
   },
@@ -194,114 +191,193 @@ export const skills: SkillGroup[] = [
 /**
  * Portfolio section
  */
-export const portfolioItems: PortfolioItem[] = [
-  {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage1,
-  },
-  {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage2,
-  },
-  {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage3,
-  },
-  {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage4,
-  },
-  {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage5,
-  },
-  {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage6,
-  },
-  {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage7,
-  },
-  {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage8,
-  },
-  {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage9,
-  },
-  {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage10,
-  },
-  {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage11,
-  },
-];
+export const portfolioItems: PortfolioItem[] = [] 
+// [
+//   {
+//     title: 'Project title 1',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://reactresume.com',
+//     image: porfolioImage1,
+//   },
+//   {
+//     title: 'Project title 2',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://reactresume.com',
+//     image: porfolioImage2,
+//   },
+//   {
+//     title: 'Project title 3',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://reactresume.com',
+//     image: porfolioImage3,
+//   },
+//   {
+//     title: 'Project title 4',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://reactresume.com',
+//     image: porfolioImage4,
+//   },
+//   {
+//     title: 'Project title 5',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://reactresume.com',
+//     image: porfolioImage5,
+//   },
+//   {
+//     title: 'Project title 6',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://reactresume.com',
+//     image: porfolioImage6,
+//   },
+//   {
+//     title: 'Project title 7',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://reactresume.com',
+//     image: porfolioImage7,
+//   },
+//   {
+//     title: 'Project title 8',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://reactresume.com',
+//     image: porfolioImage8,
+//   },
+//   {
+//     title: 'Project title 9',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://reactresume.com',
+//     image: porfolioImage9,
+//   },
+//   {
+//     title: 'Project title 10',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://reactresume.com',
+//     image: porfolioImage10,
+//   },
+//   {
+//     title: 'Project title 11',
+//     description: 'Give a short description of your project here.',
+//     url: 'https://reactresume.com',
+//     image: porfolioImage11,
+//   },
+// ];
 
 /**
  * Resume section -- TODO: Standardize resume contact format or offer MDX
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
-  },
-  {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
-  },
+    date: 'November 2009',
+    location: 'EAFIT University',
+    title: 'B. Sc. Computer Science',
+    content: <p>Strong focus on algorithms, networking and software modeling</p>,
+  }
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: '2020 - Present',
+    location: 'Atlas Health',
+    title: 'Principal Engineer',
     content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
+      <div>
+        <ul>
+          <li>Re-architected the system components to allow rapid growth</li>
+          <li>Automated the release process to allow continous updates across different components without disruptions</li>
+          <li>Lead a Penetration testing using an outsourcing company</li>
+          <li>Increased the engineering team size by 200%</li>
+          <li>Improved the standards for quality assurance across the Engineering department</li>
+          <li>Implemented SCRUM for the development process</li>
+          <li>Developed a REST API using Python and MySQL</li>
+          <li>Lead the re-design of the Typescript/React SPA</li>
+          <li>Improved the existing ETL to reduce issues</li>
+          <li>Implemented a robust CI/CD solution</li>
+        </ul>
+        <br/>
+        <i>Technologies: Docker, Google Cloud, MySQL, Python, Typescript, Spark</i>
+      </div>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: '2016 - 2020',
+    location: 'Toptal',
+    title: 'Senior FullStack Engineer',
     content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
+      <div>
+        <p>
+          Worked with multiple clients using the Toptal agency as intermediary. These clients include companies like: AdMass, Apps4gaps, Qbrio, NodeHost, Backstage and Rubrik.
+        </p>
+        <br/>
+        <i>Technologies: Python, Typescript, React, PostgreSQL, MongoDB</i>
+      </div>
+    ),
+  },
+  {
+    date: '2014 - 2016',
+    location: 'RealMassive',
+    title: 'Lead Backend Developer',
+    content: (
+      <div>
+        <ul>
+          <li>High available architecture development and design.</li>
+          <li>Backend API design and implementation to support web and mobile clients. • Continuous deployment config and operation (GitHub + CircleCI).</li>
+          <li>Scrum-like life-cycle implementation. (Jira + GitHub)</li>
+          <li>Single page app with angular.js</li>
+        </ul>
+        <br/>
+        <i>Technologies: Python, Flask, PostgreSQL, Google App Engine.</i>
+      </div>
+    ),
+  },{
+    date: '2012 - 2014',
+    location: 'ScoreMat',
+    title: 'CTO and Co-Founder',
+    content: (
+      <ul>
+        <li>Web Development using python with Google App Engine, jinja2 and webapp2</li>
+        <li>Automated testing</li>
+        <li>API developed using Google Endpoints to handle the further work with iOS and
+Android applications. Currently used from Javasacript</li>
+      </ul>
+    ),
+  },{
+    date: '2010 - 2014',
+    location: 'Paymentez',
+    title: 'Lead Developer',
+    content: (
+      <div>
+        <p>
+          Lead the development of the web platform and transactions engine from scratch for processing micro-payments of virtual goods. Currently it is integrated with major payment methods (online, offline and mobile) in Brazil, Mexico, Colombia, Chile, Argentina and Venezuela.
+        </p>
+        <br/>
+        <i>Technologies: Python, Django, MySQL, AWS, Google App Engine.</i>
+      </div>
+    ),
+  },{
+    date: '2009 - 2010',
+    location: 'Mentez SAS',
+    title: 'Web Developer',
+    content: (
+      <div>
+        <lu>
+          <li>Developed several web applications on Orkut and Facebook</li>
+          <li>Developed (Paymentez), a micro payments platform for virtual goods in Orkut, Facebook and Websites.</li>
+        </lu>
+        <br/>
+        <i>Technologies: Google App Engine, Ruby on Rails and Amazon Web Services</i>
+      </div>
+    ),
+  },
+  {
+    date: '2007 - 2009',
+    location: 'Fluid Attacks',
+    title: 'Projects Engineer',
+    content: (
+      <lu>
+        <li>Web development using Django Python Web framework</li>
+        <li>Process Compliance: ISO 2007, ISO 2000, CMMI</li>
+        <li>Deployment of IT services for different clients using GNU/Debian and Xen</li>
+        <li>Security Outsourcing for Bancolombia</li>
+      </lu>
     ),
   },
 ];
@@ -335,28 +411,28 @@ export const testimonial: TestimonialSection = {
  */
 
 export const contact: ContactSection = {
-  headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  headerText: 'Get in touch',
+  description: 'Feel free to reach out any of the following channels:',
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'jairo.vasquez@gmail.com',
+      href: 'mailto:jairo.vasquez@gmail.com',
     },
     {
-      type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      type: ContactType.LinkedIn,
+      text: 'linkedin.com/in/javamo',
+      href: 'https://www.linkedin.com/in/javamo',
     },
     {
       type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      text: '@jairovasquezm',
+      href: 'https://www.instagram.com/jairovasquezm/',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'javamo',
+      href: 'https://github.com/javamo',
     },
   ],
 };
@@ -365,9 +441,8 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/javamo'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/javamo/'},
+  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/jairovasquezm/'},
+  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/javamo'},
 ];
